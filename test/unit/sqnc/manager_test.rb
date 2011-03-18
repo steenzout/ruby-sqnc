6fda2c2ffb0c531bae2b32304129232cc818a1bc
steenzout <pedro.salgado@me.com>
2011-03-17 22:28:44 -0600

    added test cases.

--- test/unit/sqnc/manager_test.rb
+++ test/unit/sqnc/manager_test.rb
@@ -13,22 +13,86 @@ require 'lib/steenzout-sqnc'
 class TestSequenceManager < Test::Unit::TestCase
 
   def setup
+
     @config = Steenzout::ConfigurationManager.configuration_for_gem 'steenzout-sqnc'
+
+    # default sequence manager
+    @config[:implementation] = :file
+
+    # add value to a sequence file
+    File.open(@config[:sequences][:seq3][:location], 'w') {|f| f.write('111')}
+
   end
 
   def teardown
+
     @config[:sequences].each_key {|name|
       sequence_file = @config[:sequences][name][:location]
       File.delete sequence_file if File.exist? sequence_file
     }
+
   end
 
 
 
-  def __do_increment_tests
+  def test_increment
 
-    assert_equal 1, Steenzout::SequenceManager::increment(:seq1), @config[:implementation]
-    assert_equal 2, Steenzout::SequenceManager::increment(:seq1), @config[:implementation]
+    # run tests for all implementations
+    Steenzout::SequenceManager.implementations.each {|implementation|
+
+      # override configuration
+      @config[:implementation] = implementation
+
+      # load new sequence manager implementation
+      Steenzout::SequenceManager.load
+
+      # run tests
+      # no step and no offset defined
+      sequence_file1 = @config[:sequences][:seq1][:location]
+
+      assert_equal 1, Steenzout::SequenceManager::increment(:seq1), @config[:implementation]
+      assert_equal 1, File.read(sequence_file1).to_i
+
+      assert_equal 2, Steenzout::SequenceManager::increment(:seq1), @config[:implementation]
+      assert_equal 2, File.read(sequence_file1).to_i
+
+
+      # step=100 and offset=100
+      sequence_file2 = @config[:sequences][:seq2][:location]
+
+      assert_equal 200, Steenzout::SequenceManager::increment(:seq2), @config[:implementation]
+      assert_equal 200, File.read(sequence_file2).to_i
+
+      assert_equal 300, Steenzout::SequenceManager::increment(:seq2), @config[:implementation]
+      assert_equal 300, File.read(sequence_file2).to_i
+    }
+
+  end
+
+
+
+  def test_increment_with_file_containing_initial_value
+
+    # override configuration
+    @config[:implementation] = :file
+
+    # load new sequence manager implementation
+    Steenzout::SequenceManager.load
+
+    # run tests
+    sequence_file3 = @config[:sequences][:seq3][:location]
+
+    assert_equal 112, Steenzout::SequenceManager::increment(:seq3), @config[:implementation]
+    assert_equal 112, File.read(sequence_file3).to_i
+
+    assert_equal 113, Steenzout::SequenceManager::increment(:seq3), @config[:implementation]
+    assert_equal 113, File.read(sequence_file3).to_i
+
+  end
+
+
+
+  def test_increment_with_invalid_sequence_names
 
     # invalid sequence name
     assert_raises ArgumentError do
@@ -42,30 +106,16 @@ class TestSequenceManager < Test::Unit::TestCase
 
 
 
-  def test_increment
-
-    # run tests for all implementations
-    Steenzout::SequenceManager.implementations {|implementation|
-
-      # override configuration
-      @config[:implementation] = implementation
-
-      # load new sequence manager implementation
-      Steenzout::SequenceManager.load
-
-      # run tests
-      __do_increment_tests
-    }
-
-  end
-
-
-
   def test_sequences
 
-    sequence_names = []
-    Steenzout::SequenceManager.sequences {|name| sequence_names << name}
-    assert_equal([:seq1, :seq2], sequence_names)
+    result = []
+    Steenzout::SequenceManager.sequences {|name| result << name.to_s}
+    result.sort!
+
+    expected = ['seq1', 'seq2', 'seq3']
+    expected.sort!
+    
+    assert_equal expected, result
 
   end
 
